d08baf419728f0a67ce79727d4f81da2455a1051
M W <marf.wright@gmail.com>
2025-02-20 09:27:33 +0000

    Feb 20, 2025, 10:27 AM

--- Mehrfach verzweigungen/Schleifen_Beispiel.java
+++ Mehrfach verzweigungen/Schleifen_Beispiel.java
@@ -1,3 +1,9 @@
-public class Schleifen_Beispiel {
 
+public class Schleifen_Beispiel {
+    public static void main(String[] args) {
+        // Zählschleife von 1 bis 50
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(i + ". Ich muss immer meine Hausaufgaben machen");
+        }
+    }
 }
--- Mehrfach verzweigungen/Schleifen_Beispiel2.java
+++ Mehrfach verzweigungen/Schleifen_Beispiel2.java
@@ -1,3 +1,10 @@
-public class Schleifen_Beispiel2 {
 
+public class Schleifen_Beispiel2 {
+    public static void main(String[] args) {
+        // Zählschleife von 1 bis 50
+        for (int i = 1; i <= 100; i++) {
+            
+            System.out.println(i + ". Ich muss immer meine Hausaufgaben machen");
+        }
+    }
 }
